0e1d40911cbf1f68bc17dac40fe98f6167535eb9
aleksandr <aleksandr.myashin@gmail.com>
2019-03-12 14:29:26 +0700

    add configure zsh script

---
+++ configure_zsh.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+sudo apt-get install fonts-powerline
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting                                                                                                                         
+git clone https://github.com/zsh-users/zsh-autosuggestions  
+sudo apt-get install zsh-syntax-highlighting
+
