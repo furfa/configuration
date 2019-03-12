d442ed1cc031018fda186b6cda360317706265b8
furfa <aleksandr.myashin@gmail.com>
2019-03-12 14:44:04 +0700

    update configure

--- configure_zsh.sh
+++ configure_zsh.sh
@@ -2,7 +2,9 @@
 
 sudo apt-get install fonts-powerline
 
-git clone https://github.com/zsh-users/zsh-syntax-highlighting                                                                                                                         
-git clone https://github.com/zsh-users/zsh-autosuggestions  
+git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/zsh-syntax-highlighting                                                                                                                        
+git clone https://github.com/zsh-users/zsh-autosuggestions  ~/zsh-autosuggestions
+
 sudo apt-get install zsh-syntax-highlighting
 
+git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
